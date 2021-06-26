ff21e9ebf44739a7cffd905894738ac31be30e39
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-26 14:25:57 -0600

    Make board use props from the game

--- src/index.js
+++ src/index.js
@@ -13,13 +13,6 @@ function Square (props) {
   }
   
   class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
@@ -36,8 +29,8 @@ function Square (props) {
     renderSquare(i) {
       return (
         <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
         />
       ); 
     }
@@ -75,6 +68,17 @@ function Square (props) {
   }
   
   class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true, 
+        };
+    }
+
     render() {
       return (
         <div className="game">
