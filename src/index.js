49c81933ee5620e17a50773e1eba45cea31e94a2
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-26 11:45:50 -0600

    Update and add onClick so square values can be passed

--- src/index.js
+++ src/index.js
@@ -31,7 +31,12 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return (
+        <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />
+      ); 
     }
   
     render() {
