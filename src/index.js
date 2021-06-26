ee719eb7ff57f280ad3ae254cd5322b8dd53dbb6
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-26 11:38:42 -0600

    Make squares update based off board squares array

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square value={this.state.squares[i]} />;
     }
   
     render() {
