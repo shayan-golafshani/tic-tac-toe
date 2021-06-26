41b9b1e8ab630fabfee14c018403d7b3739c37dd
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-26 14:51:21 -0600

    Begin time travel feature

--- src/index.js
+++ src/index.js
@@ -78,6 +78,19 @@ function Square (props) {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc} </button>
+                </li>
+            );
+        });
+
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -95,7 +108,7 @@ function Square (props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
